d8822c7b8056046a1a1d89148b8ce27e6a7329d6
JV <andal.jvwilliam@gmail.com>
2020-12-28 23:38:44 +0800

    added enemy projectile and player health

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -6,10 +6,18 @@ public class EnemyHealth : MonoBehaviour
 {
 
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemeyProjectilePrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
         
     }
 
@@ -17,19 +25,46 @@ public class EnemyHealth : MonoBehaviour
     void Update()
     {
 
-
+        CountDownAndShoot();
         
     }
 
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyProjectile = Instantiate(enemeyProjectilePrefab, 
+            transform.position, 
+            Quaternion.identity) as GameObject;
+
+        enemyProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        Destroy(enemyProjectile, 2);
+    }
+
+
+    /// <summary>
+    /// Initiates the damage to the Gameobject on Trigger Collision
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if(!damageDealer) { return; }
         ProcessHit(damageDealer);
     }
 
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.getDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //config params
+    [Header("Player")]
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float padding = 1f;
+    [SerializeField] int health = 200;
+
+    [Header("Projectiles")]
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float fireSpeed = 10f;
     [SerializeField] float firingInterval = 0.1f;
@@ -57,7 +61,6 @@ public class Player : MonoBehaviour
     //corouting code
     IEnumerator ContinuousFire()
     {
-
         while (true)
         {
 
@@ -70,24 +73,7 @@ public class Player : MonoBehaviour
             yield return new WaitForSeconds(firingInterval);
 
         }
-        
-
-
     }
-
-    /*    private void Fire()
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                GameObject laser = Instantiate(laserPrefab, 
-                    transform.position, 
-                    Quaternion.identity) as GameObject;
-
-                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, fireSpeed);
-
-                Destroy(laser, 2);
-            }
-        }*/
 
     /// <summary>
     /// Move Player
@@ -124,6 +110,30 @@ public class Player : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Process Damage when Enemy Projectile Hits the Player GameObject
+    /// </summary>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = 
+            other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    /// <summary>
+    /// Process Damage by reducing Health with Damage preference
+    /// </summary>
+    /// <param name="damageDealer"></param>
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.getDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
 
     private void SetUpMoveBounderies()
